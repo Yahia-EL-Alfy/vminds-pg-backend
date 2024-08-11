d9aac4c865008b159b73a13757c95c56a67ec086
Yahia-EL-Alfy <90115669+Yahia-EL-Alfy@users.noreply.github.com>
2024-08-11 15:59:27 +0300

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,6 +2,9 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail', 
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, 
   auth: {
     user: 'veemswsolution@gmail.com',
     pass: 'nuvw uhhg qtet bkvc',
