6981974e0c52fbcc0fc30348758c706cb10d0b0b
Yahia-EL-Alfy <90115669+Yahia-EL-Alfy@users.noreply.github.com>
2024-08-11 16:23:05 +0300

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,41 +1,11 @@
-// const nodemailer = require('nodemailer');
-
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail', 
-//   host: 'smtp.gmail.com',
-//   port: 587,
-//   secure: false, 
-//   auth: {
-//     user: 'veemswsolution@gmail.com',
-//     pass: 'nuvw uhhg qtet bkvc',
-//   },
-// });
-
-// const sendVerificationEmail = async (to, verificationCode) => {
-//   const mailOptions = {
-//     from: 'veemswsolution@gmail.com',
-//     to,
-//     subject: 'Email Verification',
-//     html: `<p>Please use the following code to verify your email: <strong>${verificationCode}</strong></p>`,
-//   };
-
-//   try {
-//     await transporter.sendMail(mailOptions);
-//     console.log('Verification email sent');
-//   } catch (error) {
-//     console.error('Error sending verification email:', error);
-//   }
-// };
-
-// module.exports = { sendVerificationEmail };
-
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'gmail', 
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 64305,
+  secure: false, 
+
   auth: {
     user: 'veemswsolution@gmail.com',
     pass: 'nuvw uhhg qtet bkvc',
@@ -51,17 +21,8 @@ const sendVerificationEmail = async (to, verificationCode) => {
   };
 
   try {
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error('Error sending verification email:', error);
-          reject(error);
-        } else {
-          console.log('Verification email sent:', info.response);
-          resolve(info);
-        }
-      });
-    });
+    await transporter.sendMail(mailOptions);
+    console.log('Verification email sent');
   } catch (error) {
     console.error('Error sending verification email:', error);
   }
