c66bea93d011a7c3893c1ce6bf2a2572b96b7b59
Yahia-EL-Alfy <90115669+Yahia-EL-Alfy@users.noreply.github.com>
2024-10-15 19:40:50 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,11 +9,11 @@ const authenticate = require('../middlewares/authenticate');
 
 const router = express.Router();
 
-router.post('/signup', validateSignUp, signUp);
+router.post('/signup', signUp);
 router.post('/verify', verifyEmail);
 router.post('/signup-thirdParty', signUpThirdParty);
 
-router.post('/signin', validateSignIn, signIn);
+router.post('/signin', signIn);
 
 router.delete('/delete-all-chats', authenticate, deleteUsageLogs);
 
